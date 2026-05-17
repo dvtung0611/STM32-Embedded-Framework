ee2766582b12d698901a918c91c421cc546639e8
VanTung <dvtung0611@gmail.com>
2026-05-17 23:53:28 +0700

    chore(boards): rename mcu and cpu symbols

--- Boards/STM32F4-DISC1/board.mk
+++ Boards/STM32F4-DISC1/board.mk
@@ -1,11 +1,11 @@
-MCU = STM32F407xx
-CPU = Cortex-M4
+MCU = stm32f407
+CPU = cortex-m4
 
-LINKER_SCRIPT = Core/STM32F407xx/Linker/stm32f407vgtx.ld
-STARTUP_FILE = Core/STM32F407xx/Startup/startup_stm32f407vgtx.c
+LINKER_SCRIPT = core/stm32f407/linker/stm32f407vgtx.ld
+STARTUP_FILE = core/stm32f407/startup/startup-stm32f407vgtx.c
 
 OPENOCD_TARGET = target/stm32f4x.cfg
 OPENOCD_INTERFACE = interface/stlink.cfg
 
 CPU_FLAGS = \
-    -mcpu=cortex-m4 \
+    -mcpu=$(CPU) \
