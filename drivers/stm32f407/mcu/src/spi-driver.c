dd2c3a735f2232d02f857eaa7ae5057a509fa760
VanTung <dvtung0611@gmail.com>
2026-05-21 22:15:40 +0700

    refactor(spi): use bitwise operations for all instructions

--- drivers/stm32f407/mcu/src/spi-driver.c
+++ drivers/stm32f407/mcu/src/spi-driver.c
@@ -66,21 +66,21 @@ void SPI_Init(SPI_Handle_t *pSPI_Handle)
     // Configure the communication mode
     if (BusConfig == SPI_BUSCONFIG_FULL_DUPLEX) // 2-line full duplex
     {
-        pSPIx->CR1 &= ~(1U << SPI_CR1_BIDIMODE);
-        pSPIx->CR1 &= ~(1U << SPI_CR1_RXONLY);
+        CLEAR_BIT(pSPIx->CR1, SPI_CR1_BIDIMODE);
+        CLEAR_BIT(pSPIx->CR1, SPI_CR1_RXONLY);
     }
     else if (BusConfig == SPI_BUSCONFIG_HALF_DUPLEX) // 1-line half duplex
     {
-        pSPIx->CR1 |= (1U << SPI_CR1_BIDIMODE);
-        pSPIx->CR1 &= ~(1U << SPI_CR1_RXONLY);
+        WRITE_BIT(pSPIx->CR1, SPI_CR1_BIDIMODE);
+        CLEAR_BIT(pSPIx->CR1, SPI_CR1_RXONLY);
         // TX mode: BIDIOE = 1
         // RX mode: BIDIOE = 0
         // Direction controlled at runtime
     }
     else if (BusConfig == SPI_BUSCONFIG_SIMPLEX_RX) // 2-line receive-only
     {
-        pSPIx->CR1 &= ~(1U << SPI_CR1_BIDIMODE);
-        pSPIx->CR1 |= (1U << SPI_CR1_RXONLY);
+        CLEAR_BIT(pSPIx->CR1, SPI_CR1_BIDIMODE);
+        WRITE_BIT(pSPIx->CR1, SPI_CR1_RXONLY);
     }
 
     // Configure SCLK speed
@@ -125,7 +125,7 @@ void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t DataLength)
         while (SPI_GetFlagStatus(pSPIx, SPI_FLAG_TXE) == FLAG_RESET);
 
         // Check DFF (Data Frame Format)
-        if ((pSPIx->CR1 >> SPI_CR1_DFF) & 1U)
+        if (READ_BIT(pSPIx->CR1, SPI_CR1_DFF))
         {
             // 16-bit data frame
             pSPIx->DR = *((uint16_t *)(pTxBuffer)); // Load the data into the DR
@@ -154,7 +154,7 @@ void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer, uint32_t DataLengt
         while (SPI_GetFlagStatus(pSPIx, SPI_FLAG_RXNE) == FLAG_RESET);
 
         // Check DFF (Data Frame Format)
-        if ((pSPIx->CR1 >> SPI_CR1_DFF) & 1U)
+        if (READ_BIT(pSPIx->CR1, SPI_CR1_DFF))
         {
             // 16-bit data frame
             *((uint16_t *)(pRxBuffer)) = pSPIx->DR; // Read data from the DR
@@ -178,75 +178,75 @@ void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer, uint32_t DataLengt
 void SPI_PeripheralControl(SPI_RegDef_t *pSPIx, uint8_t En_or_DI)
 {
     if (En_or_DI == ENABLE)
-        pSPIx->CR1 |= (1U << SPI_CR1_SPE);
+        WRITE_BIT(pSPIx->CR1, SPI_CR1_SPE);
     else if (En_or_DI == DISABLE)
-        pSPIx->CR1 &= ~(1U << SPI_CR1_SPE);
+        CLEAR_BIT(pSPIx->CR1, SPI_CR1_SPE);
 }
 
 
 void SPI_SSIConfig(SPI_RegDef_t *pSPIx, uint8_t En_or_DI)
 {
     if (En_or_DI == ENABLE)
-        pSPIx->CR1 |= (1U << SPI_CR1_SSI);
+        WRITE_BIT(pSPIx->CR1, SPI_CR1_SSI);
     else if (En_or_DI == DISABLE)
-        pSPIx->CR1 &= ~(1U << SPI_CR1_SSI);
+        CLEAR_BIT(pSPIx->CR1, SPI_CR1_SSI);
 }
 
 
 void SPI_SSOEConfig(SPI_RegDef_t *pSPIx, uint8_t EN_or_DI)
 {
     if (EN_or_DI == ENABLE)
-        pSPIx->CR2 |= (1U << SPI_CR2_SSOE);
+        WRITE_BIT(pSPIx->CR2, SPI_CR2_SSOE);
     else if (EN_or_DI == DISABLE)
-        pSPIx->CR2 &= ~(1U << SPI_CR2_SSOE);
+        CLEAR_BIT(pSPIx->CR2, SPI_CR2_SSOE);
 }
 
 
 uint8_t SPI_SendDataIT(SPI_Handle_t *pSPI_Handle, uint8_t *pTxBuffer, uint32_t DataLength)
 {
-    uint8_t spi_state = pSPI_Handle->TxState;
+    if (pSPI_Handle->TxState == SPI_READY)
+    {
+        SPI_RegDef_t *pSPIx = pSPI_Handle->pSPIx;
 
-    if (spi_state != SPI_READY)
-        return spi_state;
-    
-    SPI_RegDef_t *pSPIx = pSPI_Handle->pSPIx;
+        // Save TX buffer pointer and transfer length
+        pSPI_Handle->pTxBuffer = pTxBuffer;
+        pSPI_Handle->TxLength = DataLength;
 
-    // Save TX buffer pointer and transfer length
-    pSPI_Handle->pTxBuffer = pTxBuffer;
-    pSPI_Handle->TxLength = DataLength;
+        // Set BUSY_IN_TX
+        pSPI_Handle->TxState = SPI_BUSY_IN_TX;
 
-    // Set BUSY_IN_TX
-    pSPI_Handle->TxState = SPI_BUSY_IN_TX;
-    spi_state = pSPI_Handle->TxState;
+        // Enable ERRIE bit
+        WRITE_BIT(pSPIx->CR2, SPI_CR2_ERRIE);
 
-    // Enable TXEIE bit
-    pSPIx->CR2 |= (1U << SPI_CR2_TXEIE);
+        // Enable TXEIE bit
+        WRITE_BIT(pSPIx->CR2, SPI_CR2_TXEIE);
+    }
 
-    return spi_state;
+    return pSPI_Handle->TxState;
 }
 
 
 uint8_t SPI_ReceiveDataIT(SPI_Handle_t *pSPI_Handle, uint8_t *pRxBuffer, uint32_t DataLength)
 {
-    uint8_t spi_state = pSPI_Handle->RxState;
+    if (pSPI_Handle->RxState == SPI_READY)
+    {
+        SPI_RegDef_t *pSPIx = pSPI_Handle->pSPIx;
 
-    if (spi_state != SPI_READY)
-        return spi_state;
-    
-    SPI_RegDef_t *pSPIx = pSPI_Handle->pSPIx;
+        // Save RX buffer pointer and transfer length
+        pSPI_Handle->pRxBuffer = pRxBuffer;
+        pSPI_Handle->RxLength = DataLength;
 
-    // Save RX buffer pointer and transfer length
-    pSPI_Handle->pRxBuffer = pRxBuffer;
-    pSPI_Handle->RxLength = DataLength;
+        // Set BUSY_IN_RX
+        pSPI_Handle->RxState = SPI_BUSY_IN_RX;
 
-    // Set BUSY_IN_RX
-    pSPI_Handle->RxState = SPI_BUSY_IN_RX;
-    spi_state = pSPI_Handle->RxState;
+        // Enable ERRIE bit
+        WRITE_BIT(pSPIx->CR2, SPI_CR2_ERRIE);
 
-    // Enable RXNEIE bit
-    pSPIx->CR2 |= (1U << SPI_CR2_RXNEIE);
+        // Enable RXNEIE bit
+        WRITE_BIT(pSPIx->CR2, SPI_CR2_RXNEIE);
+    }
 
-    return spi_state;
+    return pSPI_Handle->RxState;
 }
 
 
@@ -255,24 +255,24 @@ void SPI_IRQHandling(SPI_Handle_t *pSPI_Handle)
     SPI_RegDef_t *pSPIx = pSPI_Handle->pSPIx;
 
     // Check for TXE
-    uint8_t temp1 = ((pSPIx->SR >> SPI_SR_TXE) & 1U);
-    uint8_t temp2 = ((pSPIx->CR2 >> SPI_CR2_TXEIE) & 1U);
+    uint8_t temp1 = READ_BIT(pSPIx->SR, SPI_SR_TXE);
+    uint8_t temp2 = READ_BIT(pSPIx->CR2, SPI_CR2_TXEIE);
     if (temp1 && temp2)
     {
         SPI_TXE_InterruptHandle(pSPI_Handle);
     }
 
     // Check for RXNE
-    temp1 = ((pSPIx->SR >> SPI_SR_RXNE) & 1U);
-    temp2 = ((pSPIx->CR2 >> SPI_CR2_RXNEIE) & 1U);
+    temp1 = READ_BIT(pSPIx->SR, SPI_SR_RXNE);
+    temp2 = READ_BIT(pSPIx->CR2, SPI_CR2_RXNEIE);
     if (temp1 && temp2)
     {
         SPI_RXNE_InterruptHandle(pSPI_Handle);
     }
 
     // Check for OVR
-    temp1 = ((pSPIx->SR >> SPI_SR_OVR) & 1U);
-    temp2 = ((pSPIx->CR2 >> SPI_CR2_ERRIE) & 1U);
+    temp1 = READ_BIT(pSPIx->SR, SPI_SR_OVR);
+    temp2 = READ_BIT(pSPIx->CR2, SPI_CR2_ERRIE);
     if (temp1 && temp2)
     {
         SPI_OVR_InterruptHandle(pSPI_Handle);
@@ -283,17 +283,18 @@ void SPI_IRQHandling(SPI_Handle_t *pSPI_Handle)
 static void SPI_TXE_InterruptHandle(SPI_Handle_t *pSPI_Handle)
 {
     SPI_RegDef_t *pSPIx = pSPI_Handle->pSPIx;
-    SPI_Config_t SPI_Config = pSPI_Handle->SPI_Config;
 
     // Transmit data
-    if (SPI_Config.SPI_DFF == SPI_DFF_16BITS)
+    if (READ_BIT(pSPIx->CR1, SPI_CR1_DFF))
     {
+        // 16-bit data frame
         pSPIx->DR = *((uint16_t *)(pSPI_Handle->pTxBuffer));
         pSPI_Handle->TxLength -= 2;
         pSPI_Handle->pTxBuffer += 2;
     }
-    else if (SPI_Config.SPI_DFF == SPI_DFF_8BITS)
+    else
     {
+        // 8-bit data frame
         pSPIx->DR = *(pSPI_Handle->pTxBuffer);
         pSPI_Handle->TxLength -= 1;
         pSPI_Handle->pTxBuffer += 1;
@@ -303,6 +304,7 @@ static void SPI_TXE_InterruptHandle(SPI_Handle_t *pSPI_Handle)
     if (pSPI_Handle->TxLength == 0)
     {
         SPI_CloseTransmission(pSPI_Handle);
+        SPI_ApplicationEventCallBack(pSPI_Handle, SPI_EVENT_TX_COMPLETE);
     }
 }
 
@@ -312,29 +314,29 @@ static void SPI_CloseTransmission(SPI_Handle_t *pSPI_Handle)
     SPI_RegDef_t *pSPIx = pSPI_Handle->pSPIx;
 
     // Disable TXE interrupt
-    pSPIx->CR2 &= ~(1U << SPI_CR2_TXEIE);
+    CLEAR_BIT(pSPIx->CR2, SPI_CR2_TXEIE);
+
     pSPI_Handle->pTxBuffer = NULL;
     pSPI_Handle->TxLength = 0;
     pSPI_Handle->TxState = SPI_READY;
-
-    SPI_ApplicationEventCallBack(pSPI_Handle, SPI_EVENT_TX_COMPLETE);
 }
 
 
 static void SPI_RXNE_InterruptHandle(SPI_Handle_t *pSPI_Handle)
 {
     SPI_RegDef_t *pSPIx = pSPI_Handle->pSPIx;
-    SPI_Config_t SPI_Config = pSPI_Handle->SPI_Config;
 
     // Receive data
-    if (SPI_Config.SPI_DFF == SPI_DFF_16BITS)
+    if (READ_BIT(pSPIx->CR1, SPI_CR1_DFF))
     {
+        // 16-bit data frame
         *((uint16_t *)(pSPI_Handle->pRxBuffer)) = pSPIx->DR;
         pSPI_Handle->RxLength -= 2;
         pSPI_Handle->pRxBuffer += 2;
     }
-    else if (SPI_Config.SPI_DFF == SPI_DFF_8BITS)
+    else
     {
+        // 8-bit data frame
         *(pSPI_Handle->pRxBuffer) = (uint8_t)(pSPIx->DR);
         pSPI_Handle->RxLength -= 1;
         pSPI_Handle->pRxBuffer += 1;
@@ -344,6 +346,7 @@ static void SPI_RXNE_InterruptHandle(SPI_Handle_t *pSPI_Handle)
     if (pSPI_Handle->RxLength == 0)
     {
         SPI_CloseReception(pSPI_Handle);
+        SPI_ApplicationEventCallBack(pSPI_Handle, SPI_EVENT_RX_COMPLETE);
     }
 }
 
@@ -353,22 +356,20 @@ void SPI_CloseReception(SPI_Handle_t *pSPI_Handle)
     SPI_RegDef_t *pSPIx = pSPI_Handle->pSPIx;
 
     // Disable RXNE interrupt
-    pSPIx->CR2 &= ~(1U << SPI_CR2_RXNEIE);
+    CLEAR_BIT(pSPIx->CR2, SPI_CR2_RXNEIE);
+
     pSPI_Handle->pRxBuffer = NULL;
     pSPI_Handle->RxLength = 0;
     pSPI_Handle->RxState = SPI_READY;
-
-    SPI_ApplicationEventCallBack(pSPI_Handle, SPI_EVENT_RX_COMPLETE);
 }
 
 
 static void SPI_OVR_InterruptHandle(SPI_Handle_t *pSPI_Handle)
-{    
+{
     // Clear OVR flag
     if (pSPI_Handle->TxState != SPI_BUSY_IN_TX)
     {
         SPI_RegDef_t *pSPIx = pSPI_Handle->pSPIx;
-
         (void)pSPIx->DR;
         (void)pSPIx->SR;
 
@@ -377,7 +378,7 @@ static void SPI_OVR_InterruptHandle(SPI_Handle_t *pSPI_Handle)
 }
 
 
-__weak void SPI_ApplicationEventCallBack(SPI_Handle_t * pSPI_Handle, uint8_t SPI_AppEventFlag)
+__weak void SPI_ApplicationEventCallBack(SPI_Handle_t *pSPI_Handle, uint8_t SPI_AppEventFlag)
 {
     (void)pSPI_Handle;
     (void)SPI_AppEventFlag;
