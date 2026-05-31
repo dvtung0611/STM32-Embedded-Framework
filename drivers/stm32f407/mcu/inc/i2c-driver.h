e7101ebc18979fa9d7a7ef03887614d95a8a970b
VanTung <dvtung0611@gmail.com>
2026-05-31 17:00:30 +0700

    feat(i2c): add .c and .h files

---
+++ drivers/stm32f407/mcu/inc/i2c-driver.h
@@ -0,0 +1,13 @@
+/**
+ * File name: i2c-driver.h
+ *
+ * Created on: 31/05/2026
+ * Author: Van Tung Dinh
+ */
+
+#ifndef INC_I2C_DRIVER_H_
+#define INC_I2C_DRIVER_H_
+
+
+
+#endif /* INC_I2C_DRIVER_H_ */
---
+++ drivers/stm32f407/mcu/src/i2c-driver.c
@@ -0,0 +1,8 @@
+/**
+ * File name: i2c-driver.c
+ *
+ * Created on: 31/05/2026
+ * Author: Van Tung Dinh
+ */
+
+
